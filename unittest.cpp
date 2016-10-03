1945369b3a81dd76d620401446b2ca828935199b
Or Ostrovsky <or.ostrovsky@gmail.com>
2016-10-03 20:59:08 +0300

    Improved testing

--- unittest.cpp
+++ unittest.cpp
@@ -33,15 +33,30 @@ TEST(ListTest, ListTestEmplace){
 	std::list<int> reference_list = {1, 2, 3, 4, 5};
 	persist::list<int> my_list(reference_list.begin(), reference_list.end());
 	ASSERT_RANGES_EQ(reference_list,my_list);
-
-	auto it1 = reference_list.begin();
-	std::advance(it1, 3);
-	reference_list.emplace(it1, 6);
-	auto it2 = my_list.begin();
-	std::advance(it2, 3);
-	auto my_list2 = my_list.emplace(it2, 6);
+	
+	{
+		auto it1 = reference_list.begin();
+		std::advance(it1, 3);
+		reference_list.emplace(it1, 6);
+	}
+	decltype(my_list) my_list2;
+	{
+		auto it = my_list.begin();
+		std::advance(it, 3);
+		my_list2 = my_list.emplace(it, 6);
+	}
 	ASSERT_RANGES_EQ(reference_list, my_list2);
 	ASSERT_NE(my_list, my_list2);
+	{
+		auto it1 = my_list.begin(), it2 = my_list2.begin();
+		ASSERT_NE(it1, it2);
+		std::advance(it1, 3);
+		std::advance(it2, 4);
+		ASSERT_EQ(it1++, it2++);
+		ASSERT_EQ(it1++, it2++);
+		ASSERT_EQ(my_list.end(), it1);
+		ASSERT_EQ(my_list2.end(), it2);
+	}
 }
 
 TEST(ListTest, ListTestErase){
@@ -49,12 +64,26 @@ TEST(ListTest, ListTestErase){
 	persist::list<int> my_list(reference_list.begin(), reference_list.end());
 	ASSERT_RANGES_EQ(reference_list,my_list);
 
-	auto it1 = reference_list.begin();
-	std::advance(it1, 3);
-	reference_list.erase(it1);
-	auto it2 = my_list.begin();
-	std::advance(it2, 3);
-	auto my_list2 = my_list.erase(it2);
+	{
+		auto it = reference_list.begin();
+		std::advance(it, 3);
+		reference_list.erase(it);
+	}
+	decltype(my_list) my_list2;
+	{
+		auto it = my_list.begin();
+		std::advance(it, 3);
+		my_list2 = my_list.erase(it);
+	}
 	ASSERT_RANGES_EQ(reference_list, my_list2);
 	ASSERT_NE(my_list, my_list2);
+	{
+		auto it1 = my_list.begin(), it2 = my_list2.begin();
+		ASSERT_NE(it1, it2);
+		std::advance(it1, 4);
+		std::advance(it2, 3);
+		ASSERT_EQ(it1++, it2++);
+		ASSERT_EQ(my_list.end(), it1);
+		ASSERT_EQ(my_list2.end(), it2);
+	}
 }
