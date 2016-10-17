f82a75de6d2054f5312936b32c06cda0ac1b27e1
Or Ostrovsky <or.ostrovsky@gmail.com>
2016-10-17 22:14:51 +0300

    Added an default initializer to node

--- detail/list/node.hpp
+++ detail/list/node.hpp
@@ -10,11 +10,12 @@ namespace list{
 	struct node{
 		using value_t = std::add_const_t<std::remove_volatile_t<DataType>>;
 		using ptr_t = std::shared_ptr<node>;
+
 		value_t data_;
-		ptr_t next_;
+		ptr_t next_ = nullptr;
 		
-		node(const DataType& d): data_{d}, next_{nullptr}{};
-		node(DataType&& d): data_{d}, next_{nullptr}{};
+		node(const DataType& d): data_{d}{};
+		node(DataType&& d): data_{d}{};
 
 		template<typename... Args>
 		inline static decltype(auto) make_node(Args&&... args){
