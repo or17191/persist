1d7bfa8a786cb4421ea5149929475ec48a447cd5
Or Ostrovsky <or.ostrovsky@gmail.com>
2016-10-21 01:50:29 +0300

    Started work on type-erasur
    * Now all nodes derive from common ancestor
    * All shared_ptrs point to that ancestor, and are cast only when a value
    	is needed.

--- detail/list/iterators.hpp
+++ detail/list/iterators.hpp
@@ -5,47 +5,49 @@
 namespace persist{
 namespace detail{
 namespace list{
-		template<typename Node>
-		class list_iterator:
-				public boost::iterator_facade<list_iterator<Node>,
-																			typename Node::value_t,
-																			boost::forward_traversal_tag>{
-			private:
-				using node_t = Node;
-			public:
-				explicit list_iterator(node_t* node): node_(node){}
-				list_iterator(){}
-				inline auto equal(const list_iterator& other) const;
-				inline const auto& dereference() const;
-				inline void increment();
+	class node_base;
 
-			private:
-				node_t* node_ = nullptr;
-		};
+	template<typename Node>
+	class list_iterator:
+			public boost::iterator_facade<list_iterator<Node>,
+																		typename Node::value_t,
+																		boost::forward_traversal_tag>{
+		private:
+			using node_t = Node;
+		public:
+			explicit list_iterator(node_base* node): node_(node){}
+			list_iterator(){}
+			inline auto equal(const list_iterator& other) const;
+			inline const auto& dereference() const;
+			inline void increment();
 
-		template<typename NodePtr>
-		class node_iterator:
-				public boost::iterator_facade<node_iterator<NodePtr>,
-																			NodePtr,
-																			boost::forward_traversal_tag>{
-			private:
-				using node_ptr_t = NodePtr;
-				using node_t = typename node_ptr_t::element_type;
-			public:
-				explicit node_iterator(node_ptr_t& node): node_(std::addressof(node)){}
-				node_iterator(){}
-				inline auto equal(const node_iterator& other) const;
-				inline auto& dereference() const;
-				inline void increment();
-				inline auto& value() const;
-				inline bool valid() const;
+		private:
+			node_base* node_ = nullptr;
+	};
 
-				inline operator list_iterator<node_t>() const;
+	template<typename NodePtr>
+	class node_iterator:
+			public boost::iterator_facade<node_iterator<NodePtr>,
+																		NodePtr,
+																		boost::forward_traversal_tag>{
+		private:
+			using node_ptr_t = NodePtr;
+			using node_t = typename node_ptr_t::element_type;
+		public:
+			explicit node_iterator(node_ptr_t& node): node_(std::addressof(node)){}
+			node_iterator(){}
+			inline auto equal(const node_iterator& other) const;
+			inline auto& dereference() const;
+			inline void increment();
+			inline bool valid() const;
 
-			private:
-				node_ptr_t* node_ = nullptr;
+			template<typename OtherNode>
+			inline operator list_iterator<OtherNode>() const;
 
-		};
+		private:
+			node_ptr_t* node_ = nullptr;
+
+	};
 
 }
 }
--- detail/list/node.hpp
+++ detail/list/node.hpp
@@ -6,19 +6,23 @@
 namespace persist{
 namespace detail{
 namespace list{
+	struct node_base{
+		using ptr_t = std::shared_ptr<node_base>;
+
+		ptr_t next_ = nullptr;
+	};
+
 	template<typename DataType>
-	struct node{
+	struct node: node_base{
 		using value_t = std::add_const_t<std::remove_volatile_t<DataType>>;
-		using ptr_t = std::shared_ptr<node>;
 
 		value_t data_;
-		ptr_t next_ = nullptr;
-		
-		node(const DataType& d): data_{d}{};
-		node(DataType&& d): data_{d}{};
+
+		node(const DataType& d): node_base{}, data_{d}{};
+		node(DataType&& d): node_base{}, data_{d}{};
 
 		template<typename... Args>
-		inline static decltype(auto) make(Args&&... args){
+		inline static std::shared_ptr<node_base> make(Args&&... args){
 			return std::make_shared<node>(
 				value_t(std::forward<Args>(args)...));
 		}
