730c36213aa6711bbc668256bd3e8581e48d8785
Or Ostrovsky <or.ostrovsky@gmail.com>
2016-10-24 20:05:22 +0300

    Change directory tree

---
+++ python_bind/common.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <python3.5/pybind11/pybind11.h>
+#include <string>
+
+
+inline std::string repr(const pybind11::object & obj){
+	return obj.attr("__repr__").cast<pybind11::object>()().cast<pybind11::str>();
+}
+
+inline bool eq(const pybind11::object & lhs, const pybind11::object & rhs){
+	return lhs.attr("__eq__").cast<pybind11::object>()(rhs).cast<pybind11::bool_>();
+}
+
+inline bool ne(const pybind11::object & lhs, const pybind11::object & rhs){
+	return !eq(lhs, rhs);
+}
+
+class obj_iterator: public pybind11::iterator{
+public:
+	using pybind11::iterator::iterator;
+	inline object operator*() const{
+		return pybind11::object(*static_cast<const pybind11::iterator&>(*this), true);
+	}
+};
---
+++ python_bind/list.cpp
@@ -0,0 +1,61 @@
+#include "list.hpp"
+#include "common.hpp"
+#include <python3.5/pybind11/pybind11.h>
+#include <python3.5/pybind11/operators.h>
+#include <sstream>
+
+namespace py=pybind11;
+using list_t=persist::list<py::object>;
+
+namespace persist{
+template<>
+bool operator==<py::object>(const list<py::object>& lhs, const list<py::object>& rhs){
+	if(lhs.size() != rhs.size()){
+		return false;
+	}
+	for(auto it_l=lhs.begin(), it_r=rhs.begin();
+				it_l!=lhs.end(); ++it_l, ++it_r){
+		if(it_l == it_r){
+			return true;
+		} else if(!eq(*it_l, *it_r)){
+			return false;
+		}
+	}
+	return true;
+}
+
+template<>
+std::ostream& operator<< <py::object>(std::ostream& stream, const list<py::object>& lst){
+	stream << "[";
+	auto pos = lst.begin(), end = lst.end();
+	if(pos!=end){
+		stream << repr(*pos);
+		++pos;
+	}
+	for(; pos!=end; ++pos){
+		stream << ", " << repr(*pos);
+	}
+	stream << "]";
+	return stream;
+}
+}
+
+void create_list(py::module& m){
+	auto lst = py::class_<list_t>(m, "List");
+	lst.def(py::init<>());
+	lst.def("__init__",
+		[](list_t& instance, const py::iterable& it)
+		{ new (&instance) list_t(
+				obj_iterator(it.begin()),
+				obj_iterator(it.end()));});
+	lst.def("__len__", &list_t::size);
+	lst.def("__repr__",
+		[](const list_t& lst)
+		{std::ostringstream ret; ret << "List(" << lst << ')';
+		return ret.str();});
+	lst.def("__iter__",
+		[](const list_t& lst)
+			{return py::make_iterator(lst.begin(), lst.end());},
+		py::keep_alive<0, 1>());
+	lst.def(py::self==py::self);
+}
